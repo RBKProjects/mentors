5eadf32389da706399efc01f65379ac00bda99b6
Majd Eddin <almuhder@gmail.com>
2017-10-18 15:25:22 +0300

    (ADD) Final validation for the form

--- index.php
+++ index.php
@@ -27,16 +27,14 @@
                 </div>
                     <div class="collapse navbar-collapse" id="mydropdown">
                     <ul class="nav navbar-nav">
-                        <li><a href="http://rbk.org/#Program">The Program</a></li>
-                        <li><a href="http://rbk.org/#Curriculum">Curriculum</a></li>
-                        <li><a href="http://rbk.org/#Volunteers">Volunteers</a></li>
-                        <li><a href="http://rbk.org/#Partners">Partners</a></li>
-                        <li><a href="http://rbk.org/#hireGrads">Hire Our Grads</a></li>
-                        <li><a href="http://rbk.org/alumni-outcomes/">Alumni Outcomes</a></li>
-                        <li><a href="http://rbk.org/faqs/">FAQs</a></li>
+                        <li class="text-center"><a href="http://rbk.org/#Program">The Program</a></li>
+                        <li class="text-center"><a href="http://rbk.org/#Curriculum">Curriculum</a></li>
+                        <li class="text-center"><a href="http://rbk.org/#Volunteers">Volunteers</a></li>
+                        <li class="text-center"><a href="http://rbk.org/#Partners">Partners</a></li>
+                        <li class="text-center"><a href="http://rbk.org/alumni-outcomes/">Alumni Outcomes</a></li>
+                        <li class="text-center"><a href="http://rbk.org/faqs/">FAQs</a></li>
                     </ul>
-                    <button class="btn btn-sample navbar-btn navbar-right" onclick="window.open('http://rbkadmissions.herokuapp.com/#/','_self','resizable=yes')">Apply</button>
-
+                    <button id="applyButton" class="btn btn-sample navbar-btn navbar-right" onclick="window.open('http://rbkadmissions.herokuapp.com/#/','_self','resizable=yes')">Apply</button>
                </div>
            </div>
         </nav>
@@ -53,20 +51,20 @@
 
             <form class="form-horizontal" method="GET" action="send.php" style="padding: 20px;">
                 <div class="form-group">
-                    <label for="firstName" class="col-sm-2 control-label">First Name</label>
+                    <label for="firstName" class="col-sm-2 control-label">First Name<span class="fieldRequired"> *</span></label>
                     <div class="col-sm-10">
                         <input type="text" class="form-control" id="firstName" name="firstName" placeholder="First Name" required>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="lastName" class="col-sm-2 control-label">Last Name</label>
+                    <label for="lastName" class="col-sm-2 control-label">Last Name<span class="fieldRequired"> *</span></label>
                     <div class="col-sm-10">
                         <input type="text" class="form-control" id="lastName" name="lastName" placeholder="Last Name" required>
                     </div>
                 </div>
 
                 <div class="form-group">
-                    <label for="email" class="col-sm-2 control-label">Email</label>
+                    <label for="email" class="col-sm-2 control-label">Email<span class="fieldRequired"> *</span></label>
                     <div class="col-sm-10">
                         <input type="email" class="form-control" id="email" name="email" placeholder="Email" required>
                     </div>
@@ -78,67 +76,67 @@
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="location" class="col-sm-2 control-label">Location (Country)</label>
+                    <label for="location" class="col-sm-2 control-label">Location (Country)<span class="fieldRequired"> *</span></label>
                     <div class="col-sm-10">
                         <input type="text" class="form-control" id="location" name="location" placeholder="Location" required>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="title" class="col-sm-2 control-label">Title/Profession</label>
+                    <label for="title" class="col-sm-2 control-label">Title/Profession <span class="fieldRequired"> *</span></label>
                     <div class="col-sm-10">
                         <input type="text" class="form-control" id="title" name="title" placeholder="Title/Profession" required>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="company" class="col-sm-2 control-label">Institution / Company</label>
+                    <label for="company" class="col-sm-2 control-label">Institution / Company<span class="fieldRequired"> *</span></label>
                     <div class="col-sm-10">
                         <input type="text" class="form-control" id="company" name="company" placeholder="Institution / Organization / Company" required>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="col-sm-2 control-label">How many years of experience you have?</label>
+                    <label class="col-sm-2 control-label">How many years of experience you have?<span class="fieldRequired"> *</span></label>
                     <div style=margin-left:15px;>
                         <div class="radio">
                             <label>
-                                <input type="radio" id="zeroTwo" name="manyYears" value="0-2">
+                                <input type="radio" id="zeroTwo" name="manyYears" value="0-2" required>
                                 0 - 2 years
                             </label>
                         </div>
                         <div class="radio">
                             <label>
-                                <input type="radio" id="twoFive" name="manyYears" value="2-5">
+                                <input type="radio" id="twoFive" name="manyYears" value="2-5" required>
                                 2 - 5 years
                             </label>
                         </div>
                         <div class="radio">
                             <label>
-                                <input type="radio" id="moreFive" name="manyYears" value="5+">
+                                <input type="radio" id="moreFive" name="manyYears" value="5+" required>
                                 5+ years
                             </label>
                         </div>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="col-sm-2 control-label">Interested in mentoring more than one student?</label>
+                    <label class="col-sm-2 control-label">Interested in mentoring more than one student?<span class="fieldRequired"> *</span></label>
                         <label class="radio-inline">
-                            <input type="radio" id="yes" name="interested" value="true">
+                            <input type="radio" id="yes" name="interested" value="true" required>
                             Yes
                         </label>
                         <label class="radio-inline">
-                            <input type="radio" id="no" name="interested" value="false">
+                            <input type="radio" id="no" name="interested" value="false" required>
                             No
                         </label>
                 </div>
                 <div class="form-group">
-                    <label for="company" class="col-sm-2 control-label">Your interests (Technical and Non-Technical) </label>
+                    <label for="company" class="col-sm-2 control-label">Your interests (Technical and Non-Technical)<span class="fieldRequired"> *</span> </label>
                     <div class="col-sm-10">
-                        <textarea class="form-control" name="techNonTech" rows="3"></textarea>
+                        <textarea class="form-control" name="techNonTech" rows="3" required></textarea>
                     </div>
                 </div>
 
                 <div class="form-group">
                     <div class="col-sm-offset-2 col-sm-10">
-                            <button type="submit" class="btn btn-sample">Submit</button>
+                            <button id="submitButton" type="submit" class="btn btn-sample">Submit</button>
                     </div>
                 </div>
             </form>
@@ -146,9 +144,7 @@
     </div>
 
     <div class="contianer-fluid footer">
-        <div class="container">
-                <p class="text-center">RBK 2016 © All Rights Reserved</p>
-        </div>
+        <p class="text-center">RBK 2016 © All Rights Reserved</p>
     </div>
 
 
@@ -157,6 +153,27 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
         crossorigin="anonymous"></script>
+
+    <script>
+        function checkWidth(init) {
+            if ($(window).width() < 768) {
+                $('#submitButton').addClass('btn-block');
+                $('#applyButton').removeClass('navbar-right');
+                $('#applyButton').addClass('btn-block');
+            } else {
+                if (!init)
+                    $('#submitButton').removeClass('form-control');
+                    $('#applyButton').addClass('navbar-right');
+                    $('#applyButton').removeClass('btn-block');
+            }
+        }
+        $(document).ready(function() {
+            checkWidth(true);
+            $(window).resize(function() {
+                checkWidth(false);
+            });
+        });
+    </script>
 </body>
 
 </html>
--- send.php
+++ send.php
@@ -1,5 +1,57 @@
 <?php include 'include/functions.php'; ?>
-<?php 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>RBK | Mentor</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="_css/styles.css">
+    <link rel="shortcut icon" href="http://rbk.org/wp-content/uploads/2016/03/fav.png" />
+</head>
+
+<body>
+
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+               <div class="navbar-header">
+                    <a href="http://rbk.org" class="navbar-brand">
+                        <img class="img-responsive logo_header" src="_images/rbk_logo.png" alt="RBK logo"></a>
+                    <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mydropdown">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                    <div class="collapse navbar-collapse" id="mydropdown">
+                    <ul class="nav navbar-nav">
+                        <li class="text-center"><a href="http://rbk.org/#Program">The Program</a></li>
+                        <li class="text-center"><a href="http://rbk.org/#Curriculum">Curriculum</a></li>
+                        <li class="text-center"><a href="http://rbk.org/#Volunteers">Volunteers</a></li>
+                        <li class="text-center"><a href="http://rbk.org/#Partners">Partners</a></li>
+                        <li class="text-center"><a href="http://rbk.org/#hireGrads">Hire Our Grads</a></li>
+                        <li class="text-center"><a href="http://rbk.org/alumni-outcomes/">Alumni Outcomes</a></li>
+                        <li class="text-center"><a href="http://rbk.org/faqs/">FAQs</a></li>
+                    </ul>
+                    <button class="btn btn-sample navbar-btn navbar-right" onclick="window.open('http://rbkadmissions.herokuapp.com/#/','_self','resizable=yes')">Apply</button>
+               </div>
+           </div>
+        </nav>
+
+
+    <div class="container" style="padding-top:70px;">
+
+    </div>
+    <div class="container">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h1 class="panel-title text-center" style=" paddin-bottom:10px">RBK Mentor Registration</h1>
+            </div>
+
+<?php
     $first_name = $_GET['firstName'];
     $last_name = $_GET['lastName'];
     $email = $_GET['email'];
@@ -9,7 +61,7 @@
     $location = $_GET['location'];
     $many_years = $_GET['manyYears'];
     $interested = $_GET['interested'];
-    $tech_non_tech = $_GET['techNonTech']; 
+    $tech_non_tech = $_GET['techNonTech'];
 
     // connect to server & DB
     // connectDatabase();
@@ -17,7 +69,7 @@
     // Check connection
     if (mysqli_connect_error()) {
         die("Database connection failed: " . mysqli_connect_error());
-    }   
+    }
 
     // support Arabic
     mysqli_set_charset($conn,"utf8");
@@ -36,17 +88,60 @@
             .$many_years."',"
             .$interested.",'"
             .$tech_non_tech."')";
-            echo "SQL IS: " . $sql; 
-            
+            //echo "SQL IS: " . $sql;
+
     // insert record
     // insert(sql);
     if (mysqli_query($conn, $sql)) {
-        $last_id = mysqli_insert_id($conn);
-        echo "New record created successfully. Last inserted ID is: " . $last_id;
+        //$last_id = mysqli_insert_id($conn);
+        ?>
+
+        <div class="alert alert-success text-center">
+            <strong>Thank you!</strong> Your application has been sent successfully! We will contact your soon.
+        </div>
+            <p class="text-center" style="margin-top:-5px;"><a href="http://rbk.org">Click Here</a> to go back to the website.</p>
+        <?php
     } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        ?>
+        <div class="alert alert-danger">
+            <strong>Sorry!</strong> Something went wrong! Try Again!!.
+        </div>
+        <?php
+        //echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
 
     // close connection
     //closeConnection();
 ?>
+</div>
+</div>
+<div class="contianer-fluid footer navbar-fixed-bottom">
+    <p class="text-center">RBK 2016 © All Rights Reserved</p>
+</div>
+
+
+
+<!-- Libraries-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+    crossorigin="anonymous"></script>
+    <script>
+    function checkWidth(init) {
+        if ($(window).width() < 768) {
+            $('button').addClass('btn-block');
+        } else {
+            if (!init)
+                $('input').removeClass('form-control');
+        }
+    }
+
+    $(document).ready(function() {
+        checkWidth(true);
+        $(window).resize(function() {
+            checkWidth(false);
+        });
+    });
+    </script>
+</body>
+
+</html>
