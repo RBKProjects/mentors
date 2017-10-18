74af64fa08d64643d39bfe89c78fe5c8b1df6a6f
Majd Eddin <almuhder@gmail.com>
2017-10-18 16:35:17 +0300

    (ADD) Display List of Mentors

--- include/config.php
+++ include/config.php
@@ -1,10 +1,17 @@
 <?php
     // Database Infomation
-    // define("SERVER", "181.224.138.180");
-    // define("SERVER", "rbk.org");
+
+    // Remote
     define("SERVER", "localhost");
+    // define("DB_USERNAME", "rbk15109_mentor");
+    // define("DB_PASSWORD", "5XzA4P*.7_E7");
+    // Local
     define("DB_USERNAME", "root");
     define("DB_PASSWORD", "");
     // define("DB_NAME", "rbk.org:rbk15109_mentorship");
     define("DB_NAME", "rbk15109_mentorship");
-?> 
+
+
+    // define("SERVER", "181.224.138.180");
+    // define("SERVER", "rbk.org");
+?>
